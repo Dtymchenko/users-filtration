de878e38d7a790edd8c3e97aed88d76f463ab8c7
Dmytro Tymchenko <timchenkod88@gmail.com>
2023-02-18 01:06:14 +0200

    finished

--- src/components/FilterUsers/FilterUsers.tsx
+++ src/components/FilterUsers/FilterUsers.tsx
@@ -3,6 +3,7 @@ import styles from "./FilterUsers.module.scss"
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { RootState } from '../redux/store';
 import { setFilteredItems } from '../redux/slices/mainSlice';
+import { IItem } from './../interface';
 
 
 const FilterUsers = () => {
@@ -11,15 +12,68 @@ const FilterUsers = () => {
 
   const items = useSelector((state: RootState) => state.main.items)
   const filteredItems = useSelector((state: RootState) => state.main.filteredItems)
-  console.log(filteredItems)
+  let clone:IItem[] = Object.assign([], filteredItems)
+
   const filterNameAZ = () => {
-    const sorted = filteredItems?.sort((a,b) => {
+    
+    dispatch(setFilteredItems(clone.sort((a,b) => {
       if(a.firstName.toLowerCase() < b.firstName.toLowerCase()) return -1;
       if(a.firstName.toLowerCase() > b.firstName.toLowerCase()) return 1;
       return 0;
-    })
-    console.log(sorted)
-    // dispatch(setFilteredItems(sorted))
+    })))
+  }
+
+  const filterNameZA = () => {
+    
+    dispatch(setFilteredItems(clone.sort((a,b) => {
+      if(a.firstName.toLowerCase() < b.firstName.toLowerCase()) return 1;
+      if(a.firstName.toLowerCase() > b.firstName.toLowerCase()) return -1;
+      return 0;
+    })))
+  }
+
+  const sortAgeMinToMax = () => {
+    dispatch(setFilteredItems(clone.sort((a,b) => {
+      if(a.age < b.age) return -1;
+      if(a.age > b.age) return 1;
+      return 0;
+    })))
+  }
+
+  const sortAgeMaxToMin = () => {
+    dispatch(setFilteredItems(clone.sort((a,b) => {
+      if(a.age < b.age) return 1;
+      if(a.age > b.age) return -1;
+      return 0;
+    })))
+  }
+
+  const filterGenderAll = () => {
+    dispatch(setFilteredItems(items))
+  }
+
+  const filterGenderMale = () => {
+    dispatch(setFilteredItems(items.filter((el) => {return el.gender.toLowerCase() === "male"})))
+}
+
+  const filterGenderFemale = () => {
+    dispatch(setFilteredItems(items.filter((el) => {return el.gender.toLowerCase() === "female"})))
+  }
+
+  const filterAgeAll = () => {
+    dispatch(setFilteredItems(items))
+  }
+
+  const filterAgeOlder = () => {
+    dispatch(setFilteredItems(items.filter((el) => {return el.age >=30})))
+}
+
+const filterAgeYounger = () => {
+  dispatch(setFilteredItems(items.filter((el) => {return el.age <=30})))
+}
+
+  const setDefault = () => {
+    dispatch(setFilteredItems(items))
   }
 
   return (
@@ -29,39 +83,39 @@ const FilterUsers = () => {
         <button onClick={filterNameAZ}>
         name A-Z
         </button>
-        <button>
+        <button onClick={filterNameZA}>
         name Z-A
         </button>
         <br />
-        <button>
+        <button onClick={sortAgeMinToMax}>
         age from min to max
         </button>
-        <button>
+        <button onClick={sortAgeMaxToMin}>
         age from max to min
         </button>
         </div>
         Filter by gender:
         <div className={styles.sort_buttons}>
-        <button>
+        <button onClick={filterGenderAll}>
         All
         </button>
-        <button>
+        <button onClick={filterGenderMale}>
         Male
         </button>
-        <button>
+        <button onClick={filterGenderFemale}>
         Female
         </button>
         </div>
         Filter by age:
         <div className={styles.sort_buttons}>
-            <input type="radio" id='all' value="all" name="ageSort" defaultChecked />
+            <input type="radio" id='all' value="all" name="ageSort" defaultChecked onClick={filterAgeAll}/>
             <label htmlFor="30+">All</label>
-            <input type="radio" id='30+' value="30+" name="ageSort" />
+            <input type="radio" id='30+' value="30+" name="ageSort" onClick={filterAgeOlder}/>
             <label htmlFor="30+">30+</label>
-            <input type="radio" id='30-' value="30-" name="ageSort" />
+            <input type="radio" id='30-' value="30-" name="ageSort" onClick={filterAgeYounger}/>
             <label htmlFor="30+">30-</label>
         </div>
-        <button className={styles.default_btn}>
+        <button className={styles.default_btn} onClick={setDefault}>
             DEFAULT
         </button>
     </div>
